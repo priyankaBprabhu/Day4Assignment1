408ede7478f40cf458d9f10827342eab68269f95
priyanka B P <priyankaprabhu96@gmail.com>
2022-05-22 00:19:26 +0530

    Added part time employee and wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -8,9 +8,12 @@ public class EmployeeWage {
         System.out.println(employee);
         if (employee == 1) {
             System.out.println("Employee is present");
-            int full_time_salary = wage.dailyWage(20, 8);
-            System.out.println("full time employee daily wage" + " " + full_time_salary);
-        } else {
+            int full_time_emp_wage = wage.dailyWage(20, 8);
+            System.out.println("full time employee daily wage" + " " + full_time_emp_wage);
+            int part_time_emp_wage = wage.dailyWage(20, 4);
+            System.out.println("part time employee daily wage" + " " + part_time_emp_wage);
+        }
+        else {
             System.out.println("Employee is absent");
         }
     }
