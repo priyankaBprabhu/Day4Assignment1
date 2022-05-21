15283eaf3ddb5143751aadaba092c31c00169cfc
priyanka B P <priyankaprabhu96@gmail.com>
2022-05-21 13:47:24 +0530

    Added code to calculate full time employee daily wage

---
+++ DailyEmployeeWage.java
@@ -0,0 +1,7 @@
+package com.BridgelLabs;
+
+public class DailyEmployeeWage {
+    int dailyWage(int wage_per_hour, int hour_per_day) {
+        return (wage_per_hour * hour_per_day);
+    }
+}
--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,11 +2,14 @@ package com.BridgelLabs;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+        DailyEmployeeWage wage = new DailyEmployeeWage();
         System.out.println("Welcome to Employee Wage Computation Program ");
         int employee = (int) ((Math.random() * 10) % 2);
         System.out.println(employee);
         if (employee == 1) {
             System.out.println("Employee is present");
+            int full_time_salary = wage.dailyWage(20, 8);
+            System.out.println("full time employee daily wage" + " " + full_time_salary);
         } else {
             System.out.println("Employee is absent");
         }
