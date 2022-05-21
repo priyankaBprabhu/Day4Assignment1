9beaf86fc55f651abf1d872652d773230ff65e5c
priyanka B P <priyankaprabhu96@gmail.com>
2022-05-21 12:45:45 +0530

    Added code to check employee prsent or not

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,7 +3,12 @@ package com.BridgelLabs;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program ");
-
+        int employee = (int) ((Math.random() * 10) % 2);
+        System.out.println(employee);
+        if (employee == 1) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
-
 }
