e5d5de382668788836501711e29a48027d0b32f8
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-10 15:16:31 +0530

    Added code to calculate the wage for multiple companies

--- src/com/bridgelabz/wageComputation/EmployeeMonthlyWage.java
+++ src/com/bridgelabz/wageComputation/EmployeeMonthlyWage.java
@@ -1,21 +1,19 @@
 package com.bridgelabz.wageComputation;
 
 public class EmployeeMonthlyWage {
-    int partTimeWorkingHours = 0;
-    int fullTimeWorkingHours = 0;
-    int working_days = 0;
-    int partTimeEMPWage = 0;
-    int fullTimeEMPWage = 0;
+    private static int partTimeWorkingHours ;
+    private static int fullTimeWorkingHours ;
+    private static int working_days ;
+    private static int partTimeEMPWage;
+    private static int fullTimeEMPWage ;
+    public static int companyWage ;
 
-    int fullTimeWage(int wage_per_hour, int hour_per_day) {
-
-        return (wage_per_hour * hour_per_day );
-    }
-    int partTimeWage(int wage_per_hour, int hour_per_day) {
+    static int  wage(int wage_per_hour, int hour_per_day) {
 
         return (wage_per_hour * hour_per_day );
     }
-    public EmployeeMonthlyWage( String company, int total_working_hrs, int total_working_days, int wage_per_hour) {
+
+       public  EmployeeMonthlyWage(String company, int total_working_hrs, int total_working_days, int wage_per_hour){
         int working_hours = 0;
 
         while((working_hours <=total_working_hrs)&&(working_days <=total_working_days))
@@ -27,23 +25,24 @@ public class EmployeeMonthlyWage {
                 case 0 -> {
                     working_hours = working_hours + 8;
                     fullTimeWorkingHours = fullTimeWorkingHours + 8;
-                    fullTimeEMPWage = fullTimeWage(wage_per_hour, fullTimeWorkingHours);
+                    fullTimeEMPWage = wage(wage_per_hour, fullTimeWorkingHours);
                 }
                 case 1 -> {
                     working_hours = working_hours + 4;
                     partTimeWorkingHours = partTimeWorkingHours + 4;
-                    partTimeEMPWage = partTimeWage(wage_per_hour, partTimeWorkingHours);
+                    partTimeEMPWage = wage(wage_per_hour, partTimeWorkingHours);
                 }
                 case 2 -> {
                 }
             }
-            working_days++;
+             companyWage = (fullTimeEMPWage + partTimeEMPWage);
+             working_days++;
 
         }
-        System.out.println("Company "+ company);
+
         System.out.println(" part time employee working hours" + " " + partTimeWorkingHours);
         System.out.println(" full time employee working hours" + " " + fullTimeWorkingHours);
-        System.out.println("Total employee  wage per month" + " " + (fullTimeEMPWage + partTimeEMPWage));
+        System.out.println("Employee Monthly Wage of the " + company + " company is  "+ companyWage);
 
     }
 
--- src/com/bridgelabz/wageComputation/EmployeeWage.java
+++ src/com/bridgelabz/wageComputation/EmployeeWage.java
@@ -1,20 +1,14 @@
 package com.bridgelabz.wageComputation;
 
-import java.util.Scanner;
-
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program ");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("\nEnter the  companyChoice " + "\nSelect 1 for HCL " + "\nSelect 2 for Infosys " + "\nSelect 3 for IBM ");
-        int companyChoice = scan.nextInt();
-        switch (companyChoice) {
-            case 1 -> new EmployeeMonthlyWage("HCL", 100, 20, 25);
-            case 2 -> new EmployeeMonthlyWage("Infosys", 150, 30, 28);
-            case 3 -> new EmployeeMonthlyWage("IBM", 160, 30, 30);
-            default -> System.out.println("select the proper company number");
-        }
 
+        new EmployeeMonthlyWage("Hcl", 120, 15, 25);
+        new EmployeeMonthlyWage("BridgeLabz", 100, 20, 25);
+        new EmployeeMonthlyWage("Infosys", 140, 30, 28);
     }
 }
+
+
+
