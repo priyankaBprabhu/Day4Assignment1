0d31afbadaa552d7ff0bc4f49066ad55775dd119
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-06-10 19:05:00 +0530

    Refactored the code to calculate the EMP wage used Arraylist insted of array

--- src/com/bridgelabz/wageComputation/EmployeeWage.java
+++ src/com/bridgelabz/wageComputation/EmployeeWage.java
@@ -1,20 +1,26 @@
 package com.bridgelabz.wageComputation;
 
+import java.util.ArrayList;
+
 public class EmployeeWage {
 
     public static void main(String[] args) {
-        EmpWageMulCompanies[] employeeMonthlyWage = {new EmployeeMonthlyWage("Hcl", 120, 15, 25),
-                new EmployeeMonthlyWage("BL", 100, 20, 25),
-                new EmployeeMonthlyWage("Infosys", 140, 30, 28)};
+        EmployeeMonthlyWage employeeMonthlyWage = new EmployeeMonthlyWage("Hcl", 120, 15, 25);
+        EmployeeMonthlyWage employeeMonthlyWage1 = new EmployeeMonthlyWage("BL", 100, 20, 25);
+        EmployeeMonthlyWage employeeMonthlyWage2 = new EmployeeMonthlyWage("Infosys", 140, 30, 28);
 
-        for(int i = 0; i < employeeMonthlyWage.length; i++) {
-            employeeMonthlyWage[i].display();
-            System.out.println(employeeMonthlyWage[i].wageComputation());
+        ArrayList<EmployeeMonthlyWage> employeeWageArrayList = new ArrayList<>();
+        employeeWageArrayList.add(employeeMonthlyWage);
+        employeeWageArrayList.add(employeeMonthlyWage1);
+        employeeWageArrayList.add(employeeMonthlyWage2);
+        for(EmployeeMonthlyWage wage:employeeWageArrayList){
+            wage.display();
+            System.out.println(wage.wageComputation());
         }
     }
-
-
 }
+
+
 
 
 
